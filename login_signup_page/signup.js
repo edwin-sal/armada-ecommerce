a8d8155ae582846d19c05f3e97b5bbc1d2fbcc8f
Edwin Bañago <ej.banago00601@student.tsu.edu.ph>
2025-03-13 14:14:30 +0800

    Show correct warning message for failed signup.

--- login_signup_page/signup.js
+++ login_signup_page/signup.js
@@ -1,5 +1,8 @@
 import { accounts, signup } from "../account.js";
 
+let timeoutId;
+const warningMsg = document.getElementById('warning-msg');
+
 const user = JSON.parse(localStorage.getItem('user'));
 if(user) {	
 	window.location.href = '../../index.html';
@@ -17,10 +20,12 @@ signupForm.addEventListener('submit', function(event) {
 
 	if(!firstName || !lastName || !email || !password || !confirmPassword) {
 		console.error('Please fill out the form!');
+		showWarning('Please fill out the form!')
 		return;
 	}
 
 	if(password !== confirmPassword) {
+		showWarning(`Passwords don't match!`);
 		console.error(`Passwords don't match!`);
 		return;
 	}
@@ -34,3 +39,14 @@ signupForm.addEventListener('submit', function(event) {
 	// Move to home page
 	window.location.href = '../../index.html';
 });
+
+function showWarning(msg) {
+	warningMsg.innerText = msg;
+	warningMsg.classList.add('show');
+
+	clearTimeout(timeoutId);
+
+	timeoutId = setTimeout(function() {
+		warningMsg.classList.remove('show');
+	}, 3000);
+}
